75b257e072c6f7ea2b900bb1a6e48e1e45699075
Dana Gomez <akameftw13@gmail.com>
2021-03-19 16:58:39 +0100

    Classe Nativo atributos i metodos creados.

--- core/src/dam2021/mp08/uf3/conquerors/objetos/Nativo.java
+++ core/src/dam2021/mp08/uf3/conquerors/objetos/Nativo.java
@@ -1,4 +1,38 @@
 package dam2021.mp08.uf3.conquerors.objetos;
 
-public class Nativo {
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class Nativo extends Actor {
+
+    //Atributos del nativo
+    private int anchura, altura, direccion;
+    private Vector2 posicion;
+
+    //Direcciones del nativo
+    public static final int ARRIBA =1;
+    public static final int ABAJO =2;
+    public static final int QUIETO =0;
+
+    public Nativo(int anchura, int altura, float x, float y){
+        //Inicializamos los parametros del nativo
+        this.anchura = anchura;
+        this.altura = altura;
+        this.posicion = new Vector2(x,y);
+
+        //Marcamos la direccion inicial
+        this.direccion = QUIETO;
+
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
 }
